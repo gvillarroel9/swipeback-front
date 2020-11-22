2c786c34d40aa022a6435369e17e49ab1474be56
Geraldo Villarroel <gvillarroel9@gmail.com>
2020-11-22 18:30:45 -0400

    create credit card detail component

--- src/app/modules/app/modules/credit-card/credit-card.module.ts
+++ src/app/modules/app/modules/credit-card/credit-card.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CreditCardListComponent } from './credit-card-list/credit-card-list.component';
 import { CreditCardRoutingModule } from './credit-card-routing.module';
 import { CreditCardService } from '../../services/credit-card/credit-card.service';
+import { CreditCardDetailComponent } from './credit-card-detail/credit-card-detail.component';
 
 
 
 @NgModule({
-  declarations: [CreditCardListComponent],
+  declarations: [CreditCardListComponent, CreditCardDetailComponent],
   imports: [
     CommonModule,
     CreditCardRoutingModule
