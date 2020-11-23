459e1c4d2934366a54020f12ae3fe269e221eb2d
Osvani Colina <osvacolijim@gmail.com>
2020-11-23 09:50:29 -0400

    Show credit card details

--- src/app/modules/app/modules/credit-card/credit-card-detail/credit-card-detail.component.ts
+++ src/app/modules/app/modules/credit-card/credit-card-detail/credit-card-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CreditCardService } from '../../../services/credit-card/credit-card.service';
 
 @Component({
   selector: 'app-credit-card-detail',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreditCardDetailComponent implements OnInit {
 
-  constructor() { }
+  public creditCardInfo: any;
+
+  constructor(private route: ActivatedRoute, private creditCardService: CreditCardService) { }
 
   ngOnInit(): void {
+    const creditCardNumber = this.route.snapshot.params.id;
+    this.getCreditCardMovements(creditCardNumber);
+  }
+
+  public getCreditCardMovements(creditCardNumber: string){
+    this.creditCardService.getMovements(creditCardNumber).subscribe( 
+      (res) => {
+        res.transactions.reverse();
+        this.creditCardInfo = res;
+      }, (err) => {
+
+    });
   }
 
 }
