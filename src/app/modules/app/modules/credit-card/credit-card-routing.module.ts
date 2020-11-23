9aa5e46664dad14bd3cdc2759f788cf618b601de
Osvani Colina <osvacolijim@gmail.com>
2020-11-23 09:22:15 -0400

    Go to credit card details

--- src/app/modules/app/modules/credit-card/credit-card-routing.module.ts
+++ src/app/modules/app/modules/credit-card/credit-card-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CreditCardListComponent } from './credit-card-list/credit-card-list.component';
+import { CreditCardDetailComponent } from './credit-card-detail/credit-card-detail.component';
 
-const routes: Routes = [{ path: '', component: CreditCardListComponent }];
+const routes: Routes = [
+  { path: '', component: CreditCardListComponent },
+  { path: 'detail/:id', component: CreditCardDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
