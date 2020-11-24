a26f7748b228167e853f2558f467dda57da536c1
Manuel Da Silva <manueldasilva98@gmail.com>
2020-11-24 00:57:57 -0400

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/',
+  apiUrl: 'https://dev-swipebank-api.herokuapp.com/',
   encryptKey: 'swipebank!"·$.···@@#',
 };
 
